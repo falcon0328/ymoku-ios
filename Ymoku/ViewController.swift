2f96d87ee3fde009445677b4a71bb750fb838624
aseo <aseo@yahoo-corp.jp>
2019-04-09 22:26:18 +0900

    chore: github準備

---
+++ Ymoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ymoku
+//
+//  Created by aseo on 2019/04/09.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

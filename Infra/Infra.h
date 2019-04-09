5d814f6b8e8cf37295c9def5282d25e456f04b6d
aseo <aseo@yahoo-corp.jp>
2019-04-09 22:47:05 +0900

    feat: configureの設定を追加

---
+++ Infra/Infra.h
@@ -0,0 +1,19 @@
+//
+//  Infra.h
+//  Infra
+//
+//  Created by aseo on 2019/04/09.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Infra.
+FOUNDATION_EXPORT double InfraVersionNumber;
+
+//! Project version string for Infra.
+FOUNDATION_EXPORT const unsigned char InfraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infra/PublicHeader.h>
+
+
---
+++ Infra/Infra.swift
@@ -0,0 +1,18 @@
+//
+//  Infra.swift
+//  Infra
+//
+//  Created by aseo on 2019/04/09.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+public class Infra {
+    
+    public static func configure() {
+        FirebaseApp.configure()
+    }
+    
+}
